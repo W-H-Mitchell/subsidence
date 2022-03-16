0e72f411a196f369f8c67775eed1ac9073eaccbd
W-H-Mitchell <hamishmitchell@outlook.com>
2022-03-16 14:12:28 +0000

    climate interpolation update

--- scripts/climate_interp.py
+++ scripts/climate_interp.py
@@ -17,7 +17,6 @@ def InterpolateBetweenRasters(raster1, raster2, deltaT, interval, out_fp):
     test = grad * interval
     out = np.add(arr1,test)
 
-
     driver = gdal.GetDriverByName('GTiff')
     dst_ds = driver.CreateCopy(out_fp, ds, strict=0)
     dst_ds.GetRasterBand(1).WriteArray(out)
@@ -27,28 +26,24 @@ def InterpolateBetweenRasters(raster1, raster2, deltaT, interval, out_fp):
     band = None
     dst_ds = None
 
-
 InterpolateBetweenRasters("rcp85_model_2030-2039_winter-summer_rainfall.tif",
                           "rcp85_model_2060-2069_winter-summer_rainfall.tif",
-                          30, 30,
-                          "rcp85_model_2060-2069_winter-summer_rainfall_linearinterp.tif")
-"""
+                          30.,5.,"rcp85_model_2035-2044_winter-summer_rainfall_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_winter-summer_rainfall.tif",
                           "rcp85_model_2060-2069_winter-summer_rainfall.tif",
-                          30., 5., "rcp85_model_2035-2044_winter-summer_rainfall_lin.tif")
+                          30.,15.,"rcp85_model_2045-2054_winter-summer_rainfall_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_winter-summer_rainfall.tif",
                           "rcp85_model_2060-2069_winter-summer_rainfall.tif",
-                          30., 15., "rcp85_model_2045-2054_winter-summer_rainfall_lin.tif")
+                          30.,25.,"rcp85_model_2055-2064_winter-summer_rainfall_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_winter-summer_rainfall.tif",
                           "rcp85_model_2060-2069_winter-summer_rainfall.tif",
-                          30., 25., "rcp85_model_2055-2064_winter-summer_rainfall_lin.tif")
+                          30.,30.,"rcp85_model_2060-2069_winter-summer_rainfall_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_summer_tas.tif",
                           "rcp85_model_2060-2069_summer_tas.tif",
-                          30., 5., "rcp85_model_2035-2044_summer_tas_lin.tif")
+                          30.,5.,"rcp85_model_2035-2044_summer_tas_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_summer_tas.tif",
                           "rcp85_model_2060-2069_summer_tas.tif",
-                          30., 15., "rcp85_model_2045-2054_summer_tas_lin.tif")
+                          30.,15.,"rcp85_model_2045-2054_summer_tas_lin.tif")
 InterpolateBetweenRasters("rcp85_model_2030-2039_summer_tas.tif",
                           "rcp85_model_2060-2069_summer_tas.tif",
-                          30., 25., "rcp85_model_2055-2064_summer_tas_lin.tif")
-"""
+                          30.,25.,"rcp85_model_2055-2064_summer_tas_lin.tif")
